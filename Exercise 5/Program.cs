2fe4b692d082f913d69b9f97aa014b9977a8e094
BentHPedersen <benthpedersen@mail.com>
2021-02-12 10:27:16 +0100

    Add project files.

---
+++ Exercise 5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter url: ");
+            string urlStr = Console.ReadLine();
+
+            UriBuilder ub = new UriBuilder(urlStr);
+
+            Queue<Uri> initialUrls = new Queue<Uri>();
+            initialUrls.Enqueue(ub.Uri);
+
+            WebCrawler webCrawler = new WebCrawler(initialUrls, 50);
+
+            Console.WriteLine("\n\nValid links: ");
+            printLinks(webCrawler.GetVisitedUrls(), true);
+
+            Console.WriteLine("\n\nInvalid links: ");
+            printLinks(webCrawler.GetVisitedUrls(), false);
+
+            Console.ReadKey();
+        }
+
+        private static void printLinks(Dictionary<string, bool> links, bool isValid)
+        {
+            foreach (KeyValuePair<string, bool> kv in links)
+            {
+                if (kv.Value == isValid)
+                {
+                    Console.WriteLine("    {0}", kv.Key);
+                }
+            }
+        }
+    }
+}
---
+++ Exercise 5/WebCrawler.cs
@@ -0,0 +1,93 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Exercise_5
+{
+    class WebCrawler
+    {
+
+        private Queue<Uri> frontier;
+        private Dictionary<string, bool> visitedUrls;
+        private int maxLinks;
+
+        private WebClient wc;
+
+        public WebCrawler(Queue<Uri> initialUrls, int maxLinksToFind)
+        {
+            frontier = initialUrls;
+            visitedUrls = new Dictionary<string, bool>();
+            maxLinks = maxLinksToFind;
+
+            wc = new WebClient();
+            crawl();
+        }
+
+        private void crawl()
+        {
+            while (frontier.Count > 0 && visitedUrls.Count < maxLinks)
+            {
+                Uri url = frontier.Dequeue();
+                if (!visitedUrls.ContainsKey(url.ToString()))
+                {
+                    resolvePage(url);
+                }
+            }
+        }
+
+
+        private void resolvePage(Uri page)
+        {
+            string urlStr = page.ToString();
+
+            visitedUrls[urlStr] = true;
+            Console.WriteLine(visitedUrls.Count);
+            try
+            {
+                // try to download the webpage. Throws exception if the url is bad.
+                string webPage = wc.DownloadString(urlStr);
+
+                // look for links in the webpage
+                var hrefPattern = new Regex("href\\s*=\\s*(?:\"(?<url>[^\"]*)\"|(?<url>\\S+))", RegexOptions.IgnoreCase);
+
+                var urls = hrefPattern.Matches(webPage);
+
+                Uri baseUrl = new UriBuilder(page.Host).Uri;
+                foreach (Match url in urls)
+                {
+                    string newUrl = url.Groups["url"].Value.ToLower();
+
+                    Uri absoluteUrl = normalizedUrl(baseUrl, newUrl);
+
+                    if (absoluteUrl != null && !visitedUrls.ContainsKey(absoluteUrl.ToString()))
+                    {
+                        frontier.Enqueue(absoluteUrl);
+                    }
+                }
+            }
+            catch
+            {
+                // unable to load page
+                visitedUrls[page.ToString()] = false;
+            }
+        }
+
+        private Uri normalizedUrl(Uri baseUrl, string newUrl)
+        {
+            newUrl = newUrl.ToLower();
+            if (Uri.TryCreate(newUrl, UriKind.RelativeOrAbsolute, out var url))
+            {
+                return Uri.TryCreate(baseUrl, url, out var normalizedUrl) ? normalizedUrl : null;
+            }
+            return null;
+        }
+
+
+        public Dictionary<string, bool> GetVisitedUrls()
+        {
+            return new Dictionary<string, bool>(visitedUrls);
+        }
+    }
+}
